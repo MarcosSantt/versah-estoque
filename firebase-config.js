c9d15695d113d4f38587625cc359ecdf9b3c712e
Marcos <marcoseduardo07666@gmail.com>
2026-05-29 00:05:20 -0300

    Fix: Aba de login e cadastro totalmente integradas ao Firebase Auth

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,17 @@
-// firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore }  from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-import { getAuth }       from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
+// Suas credenciais do Firebase
 const firebaseConfig = {
-  apiKey:            "AIzaSyBKGpUGDPKVle-w-aOAFgfw4MSUtbb3bAk",
-  authDomain:        "versah-estoquecontrol.firebaseapp.com",
-  projectId:         "versah-estoquecontrol",
-  storageBucket:     "versah-estoquecontrol.firebasestorage.app",
+  apiKey: "AIzaSyBKGpUGDPKVle-w-aOAFgfw4MSUtbb3bAk",
+  authDomain: "versah-estoquecontrol.firebaseapp.com",
+  projectId: "versah-estoquecontrol",
+  storageBucket: "versah-estoquecontrol.firebasestorage.app",
   messagingSenderId: "387233050667",
-  appId:             "1:387233050667:web:3e816786c05a43f2c03a6e",
-  measurementId:     "G-VEWW20F14R"
+  appId: "1:387233050667:web:3e816786c05a43f2c03a6e",
+  measurementId: "G-VEWW20F14R"
 };
 
-const app  = initializeApp(firebaseConfig);
-export const db   = getFirestore(app);
+// Inicializa o Firebase
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
