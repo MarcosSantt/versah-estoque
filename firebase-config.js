ccb0588d644f8f6ee42468d16da3e60cc6f7bfa2
Marcos <marcoseduardo07666@gmail.com>
2026-05-28 23:44:09 -0300

    Feat: Sistema integrado totalmente ao Firebase com login ativo

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,16 @@
 // firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore }  from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getAuth }       from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "seu-projeto.firebaseapp.com",
-  projectId: "seu-projeto",
-  storageBucket: "seu-projeto.appspot.com",
-  messagingSenderId: "123456",
-  appId: "1:123456:web:abcdef"
+  apiKey:            "AIzaSyBKGpUGDPKVle-w-aOAFgfw4MSUtbb3bAk",
+  authDomain:        "versah-estoquecontrol.firebaseapp.com",
+  projectId:         "versah-estoquecontrol",
+  storageBucket:     "versah-estoquecontrol.firebasestorage.app",
+  messagingSenderId: "387233050667",
+  appId:             "1:387233050667:web:3e816786c05a43f2c03a6e",
+  measurementId:     "G-VEWW20F14R"
 };
 
 const app  = initializeApp(firebaseConfig);
